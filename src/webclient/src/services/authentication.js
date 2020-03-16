b6b138f8777afded2e48ed07f643cc6e6ff12a47
Atle Magnussen <atlemagnussen@gmail.com>
2020-03-16 06:59:15 +0100

    try auto renew

--- src/webclient/src/services/authentication.js
+++ src/webclient/src/services/authentication.js
@@ -6,15 +6,50 @@ const oicdConfig = {
     redirect_uri: `${rootPath}/callback.html`,
     response_type: "code",
     scope:"openid profile bankApi offline_access",
-    post_logout_redirect_uri: `${rootPath}`
+    post_logout_redirect_uri: `${rootPath}`,
+    automaticSilentRenew: true
 };
 
 class Authentication {
     
     constructor() {
         this.mgr = new Oidc.UserManager(oicdConfig);
+        this.mgr.events.addAccessTokenExpiring((m) => this.expiring(m));
+        this.mgr.events.addAccessTokenExpired((m) => this.expired(m));
+        this.mgr.events.addSilentRenewError((m) => this.renewError(m));
+        this.mgr.events.addUserSignedOut((m) => this.signedOut(m));
+        this.mgr.events.addUserLoaded((m) => this.uoaded(m));
+        this.mgr.events.addUserUnloaded((m) => this.unloaded(m));
+        this.mgr.events.addUserSessionChanged((m) => this.sessionChanged(m));
     }
-    
+    sessionChanged(msg) {
+        console.log("sessionChanged");;
+        console.log(msg);
+    }
+    loaded(msg) {
+        console.log("loaded");;
+        console.log(msg);
+    }
+    unloaded(msg) {
+        console.log("unloaded");;
+        console.log(msg);
+    }
+    signedOut(msg) {
+        console.log("signedOut");;
+        console.log(msg);
+    }
+    renewError(msg) {
+        console.log("renewError");;
+        console.log(msg);
+    }
+    expiring(msg) {
+        console.log("expiring");;
+        console.log(msg);
+    }
+    expired(msg) {
+        console.log("expired");;
+        console.log(msg);
+    }
     async isLoggedIn() {
         const user = await this.mgr.getUser();
         if (user) {
@@ -34,6 +69,10 @@ class Authentication {
     }
     async getUser() {
         const user = await this.mgr.getUser();
+        if (user && user.expired) {
+            const res = await this.mgr.startSilentRenew();
+            console.log(res);
+        }
         return user;
     }
     async getAccessToken() {
--- src/webclient/src/views/Home.svelte
+++ src/webclient/src/views/Home.svelte
@@ -7,15 +7,10 @@
 {#if $userProfile.loggedIn}
     <h3>Logged in as {$userProfile.email}</h3>
     <p>
-        <b>id_token:</b> {$userProfile.id_token}<br>
-        <b>session_state:</b> {$userProfile.session_state}<br>
-        <b>access_token:</b> {$userProfile.access_token}<br>
-        <b>expires_at:</b> {new Date($userProfile.expires_at*1000)} ({$userProfile.expires_at})<br>
-        <b>refresh_token:</b> {$userProfile.refresh_token}<br>
-        <b>scope:</b> {$userProfile.scope}<br>
-        <b>idp:</b> {$userProfile.idp}<br>
-        <b>sid:</b> {$userProfile.sid}<br>
-        <b>sub:</b> {$userProfile.sub}<br>
+        {#each Object.keys($userProfile) as field, i}
+            <b>{field}:</b> {$userProfile[field]}<br>
+        {/each}
+        <b>expires_at:</b> {new Date($userProfile.expires_at*1000)}<br>
     </p>
 {:else}
     <h3>Not logged in</h3>
