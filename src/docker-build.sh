80620f41f97db273f8a4cf72d2f96352d2f6fbeb
Atle Magnussen <atlemagnussen@gmail.com>
2020-04-15 20:32:34 +0200

    docker compose file that works with https

--- src/docker-build.sh
+++ src/docker-build.sh
@@ -5,15 +5,16 @@
 
 # run docker with https
 #dotnet dev-certs https -ep ${HOME}/.aspnet/https/authserver.pfx -p mysupersecretpassword666
+#dotnet dev-certs https -ep ${HOME}/.aspnet/https/aspnetapp.pfx -p mysupersecretpassword666
 
 # with bash
 #--entrypoint /bin/bash
 
-#docker run --rm -d \
-docker run -it --entrypoint /bin/bash \
+#docker run -it --entrypoint /bin/bash \
+docker run --rm -d \
 -p 6000:80 -p 6001:443 \
 -e ASPNETCORE_URLS="https://+;http://+" \
--e ASPNETCORE_HTTPS_PORT=8001 \
+-e ASPNETCORE_HTTPS_PORT=6001 \
 -e ASPNETCORE_Kestrel__Certificates__Default__Password="mysupersecretpassword666" \
 -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/authserver.pfx \
 -v ${HOME}/.aspnet/https:/https/ test.authserver
