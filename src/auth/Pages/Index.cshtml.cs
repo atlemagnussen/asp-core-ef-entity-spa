bce877f5a08e9219bc8c287f1ac9b28e81d06f09
Atle Magnussen <atlemagnussen@gmail.com>
2020-04-23 20:28:50 +0200

    add azure key vault demo

--- src/auth/Pages/Index.cshtml.cs
+++ src/auth/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace Test.auth.Pages
 {
@@ -24,13 +25,20 @@ namespace Test.auth.Pages
         public string AzureAdClientId { get; set; }
         public string ClientUrl { get; set; }
 
+        public string Secret1 { get; set; }
+        public string Secret2 { get; set; }
+        public string Secret3 { get; set; }
+
         public void OnGet()
         {
             ConnectionString = GetConStr("AuthDb");
             AllowedClientUrl = _configuration.GetValue<string>("AllowedClientUrl");
             AzureAdClientId = _configuration.GetValue<string>("AzureAd:ClientId");
             ClientUrl = _configuration.GetValue<string>("AllowedClientUrl");
-        }
+            Secret1 = _configuration["SecretName"];
+            Secret2 = _configuration["Section:SecretName"];
+            Secret3 = _configuration.GetSection("Section")["SecretName"];
+    }
 
         private string GetConStr(string name) {
             var connectionString = _configuration.GetConnectionString(name);
