a6c9b24c39256509e7b925ed4bc4c0fd27dbc457
Atle Magnussen <atlemagnussen@gmail.com>
2020-04-24 18:12:06 +0200

    rem up script

--- src/webapi/deploy-azure-webapp-up.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# this does not work
-APPNAME="asp-core-webapi"	#$(az webapp list --query [1].name --output tsv)
-APPRG="dotnet-core"		#$(az webapp list --query [0].resourceGroup --output tsv)
-APPPLAN="linux-north-europe"	#$(az appservice plan list --query [0].name --output tsv)
-APPSKU="B1"			#$(az appservice plan list --query [0].sku.name --output tsv)
-APPLOCATION="North Europe"	#$(az appservice plan list --query [0].location --output tsv)
-az webapp up --name $APPNAME --resource-group $APPRG --plan $APPPLAN --sku $APPSKU --location "$APPLOCATION"
