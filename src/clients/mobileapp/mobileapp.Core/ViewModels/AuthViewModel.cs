536c0f6a66e6f8399a027e582d9a7553973d608d
Atle Magnussen <atlemagnussen@gmail.com>
2020-05-07 18:55:12 +0200

    move all into page

--- src/clients/mobileapp/mobileapp.Core/ViewModels/AuthViewModel.cs
+++ src/clients/mobileapp/mobileapp.Core/ViewModels/AuthViewModel.cs
@@ -1,16 +1,67 @@
-﻿namespace mobileapp.Core.ViewModels
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace mobileapp.Core.ViewModels
 {
     public class AuthViewModel : BaseViewModel
     {
         public AuthViewModel()
         {
+            LogoutCommand = new Command(async () =>
+            {
+                var result = await App.AuthService.LogoutAsync();
+                IsLoggedIn = false;
+            });
+            AuthenticateCommand = new Command(async () =>
+            {
+                var result = await App.AuthService.LoginAsync();
+                IsLoggedIn = result.LoggedIn;
+                //MessagingCenter.Send(this, "AuthenticationUpdated", result);
+                App.DataStore.SetBearerToken(result.AccessToken);
+            });
         }
 
         public void Refresh()
         {
             var currentState = App.AuthService.GetCurrentState();
             IsLoggedIn = currentState.LoggedIn;
+            UserName = currentState.UserName;
+            AccessToken = currentState.AccessToken;
+            IdToken = currentState.IdentityToken;
+            RefreshToken = currentState.RefreshToken;
         }
+
+        private string userName;
+        public string UserName
+        {
+            get { return userName; }
+            set { SetProperty(ref userName, value); }
+        }
+
+        private string accessToken;
+        public string AccessToken
+        {
+            get { return accessToken; }
+            set { SetProperty(ref accessToken, value); }
+        }
+
+        private string idToken;
+        public string IdToken
+        {
+            get { return idToken; }
+            set { SetProperty(ref idToken, value); }
+        }
+
+        private string refreshToken;
+        public string RefreshToken
+        {
+            get { return refreshToken; }
+            set { SetProperty(ref refreshToken, value); }
+        }
+        
+
+        public ICommand AuthenticateCommand { get; }
+        public ICommand LogoutCommand { get; }
 
         public bool NotLoggedIn
         {
